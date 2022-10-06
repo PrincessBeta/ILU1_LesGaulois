275f86b939e3d05635630d072e99b067c54b22e5
PrincessBeta <Princessbranlobein@pm.me>
2022-10-06 15:04:08 +0200

    fin TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipements = 0;
 	
 	public Romain(String nom, int force) {
 		assert force>=0; 
 		this.nom = nom;
 		this.force = force;
+		
 	}
 	public String getNom() {
 		return nom;
@@ -27,11 +30,42 @@ public class Romain {
 		} else {
 			parler("J'abandonne...");
 		assert forceinit<force;
+		}
 	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipements) {
+		case 2: {
+			System.out.println("le soldat "+nom+" est déjà bien protégé");
+			break;
+		}
+		case 1:{
+			if (equipements[0] == equipement) {
+				System.out.println("le soldat "+nom+" a déjà un "+equipement);
+			}
+			else {
+				equipe(equipement);
+			}
+			break;
+		}
+		default:
+			equipe(equipement);
+			break;
+		}
 	}
+	private void equipe(Equipement equipement) {
+		equipements[0] = equipement;
+		nbEquipements++;
+		System.out.println("le soldat "+nom+" s'equipe avec un "+equipement);
+
+	}
+	
 	public static void main(String[] args) {
 		Romain romain = new Romain("romain",6);
 		romain.parler("Je suis ROMAIN");
-		romain.recevoirCoup(2);
+		romain.sEquiper(Equipement.BOUCLIER);
+		romain.sEquiper(Equipement.BOUCLIER);
+		romain.sEquiper(Equipement.CASQUE);
+		romain.sEquiper(Equipement.CASQUE);
 	}
 }
